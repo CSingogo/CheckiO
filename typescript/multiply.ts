ce3fa9f78cfb15cebe6d0a3ccaf0bebf1525c05b
CSingogo <singogosingogo@gmail.com>
2025-09-01 08:17:29 +0200

    solutions added

---
+++ typescript/multiply.ts
@@ -0,0 +1,14 @@
+// This is an intro mission, the purpose of which is to explain how to solve missions on CheckiO.
+
+// This mission is the easiest one. Write a function that will receive 2 numbers as input and it should return the multiplication of these 2 numbers.
+
+// Input: Two arguments. Both are of type int.
+
+// Output: Int.
+
+
+function multTwo(a: number, b: number): number {
+    // your code here
+    
+    return a * b;
+}
