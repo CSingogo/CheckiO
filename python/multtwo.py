1df0e4b7114a11c0696b9fe050214f746ea66b61
CSingogo <singogosingogo@gmail.com>
2026-01-16 10:01:26 +0200

    solutions added

---
+++ python/multtwo.py
@@ -0,0 +1,10 @@
+# This is an intro mission, the purpose of which is to explain how to solve missions on CheckiO. If you want to know how to get the maximum out of using CheckiO, check out our blog post "From Basic to Advance usage".
+
+# This mission is the easiest one. Write a function that will receive 2 numbers as input and it should return the multiplication of these 2 numbers.
+
+# Input: Two arguments. Both are of type int.
+
+# Output: Int.
+def mult_two(a: int, b: int) -> int:
+    # your code here
+    return a * b
